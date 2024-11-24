cd61a09fbd94825d56dcd88924554212b2f2a351
ViniciusDev <viniciusdev10@gmail.com>
2024-11-24 13:46:11 -0300

    created repository

---
+++ src/main/java/com/picpaysimplificado/repositories/UserRepository.java
@@ -0,0 +1,17 @@
+package com.picpaysimplificado.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.picpaysimplificado.domain.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(Long id);
+
+
+
+}
